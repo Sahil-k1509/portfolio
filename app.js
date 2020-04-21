a54b609917172cf573361fe974d70db0efc51f08
Sahil Bairagi <sahilbairagi319@gmail.com>
2020-04-22 00:25:08 +0530

    Added the javascript file

---
+++ app.js
@@ -0,0 +1,11 @@
+// finding length of svg to use dash array
+/*
+const logo = document.querySelectorAll('#name path');
+
+for(let i=0; i<logo.length; i++){
+  console.log(`letter ${i} is ${logo[i].getTotalLength()}`);
+}
+*/
+
+
+// Rating Initialization
